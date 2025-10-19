a38ecc2589e0b8eb751dbd2a92efa2c394cb9d31
parvaraval <parvaraval27@gmail.com>
2025-10-20 01:25:23 +0530

    Feat: Configure routing with React Router (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Landing from './pages/Landing';
 import Builder from './pages/Builder';
+import Home from './pages/Home';
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<Landing />} />
                     <Route path="/builder" element={<Builder />} />
+                    <Route path="/home" element={<Home />} />
                 </Routes>
             </MainLayout>
         </BrowserRouter>
